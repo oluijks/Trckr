450e6e2428d5d6946c5f987410c624510ff43fb9
Olaf Luijks <oluijks@protonmail.com>
2021-01-28 06:05:45 +0100

    Initial Commit

---
+++ Trckr/TrckrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrckrApp.swift
+//  Trckr
+//
+//  Created by Olaf Luijks on 28/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrckrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
